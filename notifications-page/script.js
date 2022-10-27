2cec3c88169877a131feb9d453118f1d0afefa02
Kristine Molina <Kristine.Molina@entaingroup.com>
2022-10-27 19:15:45 +0800

    script

--- notifications-page/script.js
+++ notifications-page/script.js
@@ -6,28 +6,32 @@
 
 // }
 
-const messages = document.querySelectorAll('.msgsWrapper');
-const icon = document.querySelectorAll('.iconClass');
+// const messages = document.querySelectorAll('.msgsWrapper');
+// // // const icon = document.querySelectorAll('.iconClass');
 
+// for (let i = 0; i < messages.length; i++){
+//     messages[i].addEventListener('click', function messagesClick(){
+//             messages[i].classList.toggle('blue');
+//     })
 
-for (let i = 0; i < messages.length; i++){
-    messages[i].addEventListener('click', 
-        function messagesClick(){
-            messages[i].classList.toggle('blue');
-    })
-
-    console.log(i, "i am bg of wrapper")
-}
-
-for (let i = 0; i < icon.length; i++){
-    icon[i].addEventListener('click', 
-        function iconClick(){
-            icon[i].classList.toggle('ikon');
-    })
-
-    console.log(i, "i am icon color")
-}
+//     // console.log(i, "i am bg of wrapper")
+// }
 
 // messages.addEventListener('click', () => {
 //     document.querySelector('.msgsWrapper').getElementsByClassName.backgroundColor = 'hsl(211, 68%, 94%)';
 // });
+
+//count after clicking on section
+
+const messages = document.querySelector('.msgsWrapper').addEventListener('click', msgsClick);
+
+var clicks = 0
+function msgsClick(){
+    clicks += 1;
+    document.getElementById('msgsCount').innerHTML = clicks;
+    document.getElementById('msgsCount').classList.add('msgsCountStyle');
+    // var element = document.getElementById('msgsCount');
+    // element.classList.add('msgsCountStyle');
+    
+    // console.log(clicks);
+}
